106a3ea9d624187ef40eaa71a87cdef0ebbd7a75
Akram Al-kohadi <akramalkohadi@gmail.com>
2025-09-15 02:33:42 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,20 +1,13 @@
 
-export const supabaseUrl = 'https://iuadrfljlspngmplaeyf.supabase.co';
-export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YWRyZmxqbHNwbmdtcGxhZXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NjAyNTgsImV4cCI6MjA3MzQzNjI1OH0.uRSbrIMpzkQQCuwqU4zk6Tb6vXgfehkVT34PBFQtlYU'; // ضع هنا المفتاح الكامل
-   const { createClient } = supabase;
-export const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
-const { data, error } = await supabaseClient
-    .from('schools') // ضع هنا اسم الجدول
-    .select('*');
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-if (error) {
-    console.error('Error fetching products:', error);
-}
-else{
-    console.log(data)
-}
+const SUPABASE_URL = 'https://iuadrfljlspngmplaeyf.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YWRyZmxqbHNwbmdtcGxhZXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NjAyNTgsImV4cCI6MjA3MzQzNjI1OH0.uRSbrIMpzkQQCuwqU4zk6Tb6vXgfehkVT34PBFQtlYU';
 
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export default supabase;
 
 
 
