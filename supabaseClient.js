8e7b3386b81be762a714721b1e16fff796c884f3
Akram Al-kohadi <akramalkohadi@gmail.com>
2025-09-15 01:31:39 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,20 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-export const SUPABASE_URL = 'https://iuadrfljlspngmplaeyf.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YWRyZmxqbHNwbmdtcGxhZXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NjAyNTgsImV4cCI6MjA3MzQzNjI1OH0.uRSbrIMpzkQQCuwqU4zk6Tb6vXgfehkVT34PBFQtlYU';
+export const supabaseUrl = 'https://iuadrfljlspngmplaeyf.supabase.co';
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YWRyZmxqbHNwbmdtcGxhZXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NjAyNTgsImV4cCI6MjA3MzQzNjI1OH0.uRSbrIMpzkQQCuwqU4zk6Tb6vXgfehkVT34PBFQtlYU'; // ضع هنا المفتاح الكامل
+   const { createClient } = supabase;
+export const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const { data, error } = await supabaseClient
+    .from('schools') // ضع هنا اسم الجدول
+    .select('*');
+
+if (error) {
+    console.error('Error fetching products:', error);
+}
+else{
+    console.log(data)
+}
+
+
 
 
